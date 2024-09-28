5687f2d71e454fc8fb8ec7a94e7402532c409f25
Bobbyshu <shu.yun@northeastern.edu>
2024-09-27 22:31:21 -0400

    impl LeafNode::fromBytes based on InnerNode::fromBytes

--- src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
+++ src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
@@ -376,8 +376,21 @@ class LeafNode extends BPlusNode {
         // Note: LeafNode has two constructors. To implement fromBytes be sure to
         // use the constructor that reuses an existing page instead of fetching a
         // brand new one.
+        Page page = bufferManager.fetchPage(treeContext, pageNum);
+        Buffer buf = page.getBuffer();
 
-        return null;
+        byte nodeType = buf.get();
+        assert(nodeType == (byte) 0);
+        List<DataBox> keys = new ArrayList<>();
+        List<Long> children = new ArrayList<>();
+        int n = buf.getInt();
+        for (int i = 0; i < n; ++i) {
+            keys.add(DataBox.fromBytes(buf, metadata.getKeySchema()));
+        }
+        for (int i = 0; i < n + 1; ++i) {
+            children.add(buf.getLong());
+        }
+        return new InnerNode(metadata, bufferManager, page, keys, children, treeContext);
     }
 
     // Builtins ////////////////////////////////////////////////////////////////
